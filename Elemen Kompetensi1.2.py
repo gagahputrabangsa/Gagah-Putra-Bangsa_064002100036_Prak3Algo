5eed150fb7e2c8917201d80efa8b49ce6b59f49c
gagahputrabangsa <91167860+gagahputrabangsa@users.noreply.github.com>
2021-10-06 08:37:36 +0700

    Add files via upload

---
+++ Elemen Kompetensi1.2.py
@@ -0,0 +1,12 @@
+print("@@@@@   @@@@@@   @@@@@   @@@@@@   @    @")
+print("@       @    @   @       @    @   @    @")
+print("@ @@@   @@@@@@   @ @@@   @@@@@@   @@@@@@")
+print("@   @   @    @   @   @   @    @   @    @")
+print("@@@@@   @    @   @@@@@   @    @   @    @")
+
+a=int(input("masukan angka awal = "))
+b=int(input("masukan angka akhir = "))
+while a<b:
+  print(a, b)
+  a = a+1
+  b = b-1
